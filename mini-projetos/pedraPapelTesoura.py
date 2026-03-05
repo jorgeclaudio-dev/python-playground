6f1ffcd1e7793c82753c948653da7d7c64b55764
Jorge Claudio <jorgeclaudiocf@gmail.com>
2026-03-05 18:03:55 -0300

    Adiciona Jogo Pedra Papel e Tesoura v1

--- mini-projetos/pedraPapelTesoura.py
+++ mini-projetos/pedraPapelTesoura.py
@@ -1,3 +1,46 @@
 import random
-pc = random.randint(0, 2)
-print(pc)
+pc_escolha = ['Pedra', 'Papel', 'Tesoura']
+pc = random.choice(pc_escolha)
+
+print('-=-' * 4, 'Pedra, Papel ou Tesoura!', '-=-' * 4)
+print(
+    '[ 0 ] Para jogar Pedra\n'
+    '[ 1 ] Para jogar Papel\n'
+    '[ 2 ] Para jogar Tesoura\n'
+)
+user = int(input('Qual sua jogada: '))
+
+if user not in [0, 1, 2]:
+    print('Escolha inválida, escolha entre 0, 1 e 2')
+
+elif user == 0: # Pedra
+    print(f'Computador jogou {pc}!')
+    print('Você jogou Pedra!')
+    if pc == 'Pedra':
+        print('EMPATE')
+    elif pc == 'Tesoura':
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADOR PERDE')
+
+elif user == 1: # Papel
+    print(f'Computador jogou {pc}!')
+    print('Jogador jogou Papel!')
+    if pc == 'Pedra':
+        print('JOGADOR VENCE')
+    elif pc == 'Tesoura':
+        print('JOGADOR PERDE')
+    else:
+        print('EMPATE')
+
+else: # Tesoura
+    print(f'Computador jogou {pc}!')
+    print('Jogador jogou Tesoura!')
+    if pc == 'Pedra':
+        print('JOGADOR PERDE')
+    elif pc == 'Papel':
+        print('JOGADOR VENCE')
+    else:
+        print('EMPATE')
+
+print('-=-' * 4, 'Fim de jogo!', '-=-' * 4)
